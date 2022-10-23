5e5a0d74fbf0119c2223438ebe524a91269c90c6
Chanwoong1 <jcw7468@gmail.com>
2022-10-23 16:37:57 +0900

    add blog project

---
+++ data/blog/blog_project.md
@@ -0,0 +1,8 @@
+---
+title: Blog Project
+date: '2022-09-21'
+tags: ['next.js', 'blog']
+draft: false
+layout: PostSimple
+summary: 'How to derive the OLS Estimator with matrix notation and a tour of math typesetting using markdown with the help of KaTeX.'
+---
--- data/projectsData.js
+++ data/projectsData.js
@@ -1,25 +1,15 @@
 const projectsData = [
   {
+    title: '블로그 페이지 만들기',
+    description: `나만의 블로그로 내 지식 공유하기`,
+    imgSrc: '/static/images/blog_project/nextjs.png',
+    href: '/blog/blog_project',
+  },
+  {
     title: '이용 고객 페르소나 정의',
     description: `서비스 이용 고객의 주된 정보들을 토대로 고객의 페르소나를 정의하는 프로젝트입니다. 이를 통해 어떤 고객들이 서비스를 이용하는지 파악하고, 해당 고객들에 대한 타겟 광고를 실시하여 이용객 증대에 도움이 될 것이라 생각했습니다.`,
     imgSrc: '/static/images/persona_project/persona_project.png',
     href: '/blog/persona_project',
-  },
-  {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
   },
 ]
 
