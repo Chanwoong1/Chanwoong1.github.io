163b67911ab02891e263d67c1e426570819da37e
Chanwoong1 <jcw7468@gmail.com>
2022-10-23 15:09:25 +0900

    add images

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: '이용 고객 페르소나 정의',
+    description: `서비스 이용 고객의 주된 정보들을 토대로 고객의 페르소나를 정의하는 프로젝트입니다. 이를 통해 어떤 고객들이 서비스를 이용하는지 파악하고, 해당 고객들에 대한 타겟 광고를 실시하여 이용객 증대에 도움이 될 것이라 생각했습니다.`,
+    imgSrc: '/static/images/persona_project/persona_project.png',
+    href: '/blog/persona_project.mdx',
+  },
+  {
     title: 'A Search Engine',
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
@@ -14,12 +20,6 @@ const projectsData = [
     forgotten headphones with this simple yet affordable solution.`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
-  },
-  {
-    title: '이용 고객 페르소나 정의',
-    description: `서비스 이용 고객의 주된 정보들을 토대로 고객의 페르소나를 정의하는 프로젝트입니다. 이를 통해 어떤 고객들이 서비스를 이용하는지 파악하고, 해당 고객들에 대한 타겟 광고를 실시하여 이용객 증대에 도움이 될 것이라 생각했습니다.`,
-    imgSrc: '/static/images/persona_project.png',
-    href: '/blog/persona_project.mdx',
   },
 ]
 
