d0d35d12183e8047d6ce183a0053f6760a278836
Chanwoong1 <jcw7468@gmail.com>
2022-10-27 09:48:11 +0900

    edit metadata - giscus

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -42,10 +42,14 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      // repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+      // repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+      // category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+      // categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'chanwoong1/chanwoong1.github.io',
+      repositoryId: 'R_kgDOIQ1Stw',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOIQ1St84CSN3B',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
@@ -56,7 +60,7 @@ const siteMetadata = {
       // Place the comment box above the comments. options: bottom, top
       inputPosition: 'bottom',
       // Choose the language giscus will be displayed in. options: en, es, zh-CN, zh-TW, ko, ja etc
-      lang: 'en',
+      lang: 'ko',
       // theme when dark mode
       darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
