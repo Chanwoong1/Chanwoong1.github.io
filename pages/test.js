b971dd4910c4d55b64d516a5ab62a43050f1c593
Chanwoong1 <jcw7468@gmail.com>
2022-10-31 20:43:25 +0900

    feat: 새로운 페이지 추가 - 우테코 과제 구현 페이지 만들어볼 예정

---
+++ pages/test.js
@@ -0,0 +1,49 @@
+import siteMetadata from '@/data/siteMetadata'
+import projectsData from '@/data/projectsData'
+import Card from '@/components/Card'
+import { PageSEO } from '@/components/SEO'
+
+export default function Test() {
+  return (
+    <>
+      <PageSEO title={`Week_1no - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
+            우테코 1주차
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            웹 페이지에 과제 구현해보기
+          </p>
+        </div>
+        <div className="container py-12">
+          <h3 className="text-2xl font-bold leading-8 tracking-tight">Problem 1</h3>
+          <p>사진이랑 링크 넣기</p>
+        </div>
+      </div>
+      {/* <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Projects
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Showcase your projects with a hero image (16 x 9)
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {projectsData.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+      </div> */}
+    </>
+  )
+}
