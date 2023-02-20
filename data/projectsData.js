983e70bf8cdcc033a056a2cbfd947e50fea7e400
chanwoong1 <jcw7468@gmail.com>
2023-02-20 11:50:51 +0900

    feat: 프로젝트 목록 제거

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,52 +1,52 @@
 const projectsData = [
-  {
-    title: '알고리즘 스터디',
-    description: `코딩 테스트를 대비하자`,
-    imgSrc: '/static/images/blog_posts/algorithm_study/project_img.png',
-    href: '/tags/algorithm-study',
-  },
-  {
-    title: '우테코 프리코스',
-    description: `4주간의 내 발자취 기록하기`,
-    imgSrc: '/static/images/blog_posts/woowacourse/woowacourse_project_img.png',
-    href: '/blog/woowacourse/precourse_main',
-  },
-  {
-    title: '블로그 페이지 만들기',
-    description: `나만의 블로그로 내 지식 공유하기`,
-    imgSrc: '/static/images/blog_posts/blog_project/nextjs.png',
-    href: '/tags/blog',
-  },
-  {
-    title: '42서울',
-    description: `본 과정 과제 탐구`,
-    imgSrc: '/static/images/blog_posts/logo42.svg',
-    href: '/tags/42seoul',
-  },
-  {
-    title: '자바스크립트 leetcode',
-    description: `많이 풀어보기`,
-    imgSrc: '/static/images/blog_posts/javascript/leetcode_logo.png',
-    href: '/tags/leetcode',
-  },
-  {
-    title: '자바스크립트 독학',
-    description: `혼자 부딪히고 함께 아파해보기`,
-    imgSrc: '/static/images/blog_posts/javascript/js_logo.png',
-    href: '/blog/javascript/js_main',
-  },
-  {
-    title: '이용 고객 페르소나 정의',
-    description: `서비스 이용 고객의 주된 정보들을 토대로 고객의 페르소나를 정의하는 프로젝트입니다. 이를 통해 어떤 고객들이 서비스를 이용하는지 파악하고, 해당 고객들에 대한 타겟 광고를 실시하여 이용객 증대에 도움이 될 것이라 생각했습니다.`,
-    imgSrc: '/static/images/blog_posts/persona_project/persona_project.png',
-    href: '/blog/persona_project',
-  },
-  {
-    title: '따릉이 데이터를 활용한 데이터 분석',
-    description: `데이터 분석 첫 걸음`,
-    imgSrc: '/static/images/blog_posts/bicycle_predict/bicycle_project.jpeg',
-    href: '/blog/dacon/bicycle_predict',
-  },
+  // {
+  //   title: '알고리즘 스터디',
+  //   description: `코딩 테스트를 대비하자`,
+  //   imgSrc: '/static/images/blog_posts/algorithm_study/project_img.png',
+  //   href: '/tags/algorithm-study',
+  // },
+  // {
+  //   title: '우테코 프리코스',
+  //   description: `4주간의 내 발자취 기록하기`,
+  //   imgSrc: '/static/images/blog_posts/woowacourse/woowacourse_project_img.png',
+  //   href: '/blog/woowacourse/precourse_main',
+  // },
+  // {
+  //   title: '블로그 페이지 만들기',
+  //   description: `나만의 블로그로 내 지식 공유하기`,
+  //   imgSrc: '/static/images/blog_posts/blog_project/nextjs.png',
+  //   href: '/tags/blog',
+  // },
+  // {
+  //   title: '42서울',
+  //   description: `본 과정 과제 탐구`,
+  //   imgSrc: '/static/images/blog_posts/logo42.svg',
+  //   href: '/tags/42seoul',
+  // },
+  // {
+  //   title: '자바스크립트 leetcode',
+  //   description: `많이 풀어보기`,
+  //   imgSrc: '/static/images/blog_posts/javascript/leetcode_logo.png',
+  //   href: '/tags/leetcode',
+  // },
+  // {
+  //   title: '자바스크립트 독학',
+  //   description: `혼자 부딪히고 함께 아파해보기`,
+  //   imgSrc: '/static/images/blog_posts/javascript/js_logo.png',
+  //   href: '/blog/javascript/js_main',
+  // },
+  // {
+  //   title: '이용 고객 페르소나 정의',
+  //   description: `서비스 이용 고객의 주된 정보들을 토대로 고객의 페르소나를 정의하는 프로젝트입니다. 이를 통해 어떤 고객들이 서비스를 이용하는지 파악하고, 해당 고객들에 대한 타겟 광고를 실시하여 이용객 증대에 도움이 될 것이라 생각했습니다.`,
+  //   imgSrc: '/static/images/blog_posts/persona_project/persona_project.png',
+  //   href: '/blog/persona_project',
+  // },
+  // {
+  //   title: '따릉이 데이터를 활용한 데이터 분석',
+  //   description: `데이터 분석 첫 걸음`,
+  //   imgSrc: '/static/images/blog_posts/bicycle_predict/bicycle_project.jpeg',
+  //   href: '/blog/dacon/bicycle_predict',
+  // },
 ]
 
 export default projectsData
